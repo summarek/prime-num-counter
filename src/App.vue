29d13df378d8e52ed1feab4a238e8897ee44896a
Robert <robert.skrzyniecki@gmail.com>
2019-06-13 19:21:08 +0200

    Initial commit.
    Here we go again.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Polish</router-link>|
+      <router-link to="/about">English</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>
+        KALKULATOR LICZB PIERWSZYCH
+        I LICZB SUPER PIERWSZYCH
+      </h1>
+    </header>
+    <section class="info"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: 20px;
+    text-align: center;
+  }
+}
+</style>
